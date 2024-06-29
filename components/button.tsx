c9ba7fe918a1f124a2ed5cad993c75b2e7a34595
RajCherukuru <chrajkishan999@gmail.com>
2024-06-29 01:38:18 -0700

    created teh nav bar

---
+++ components/button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface buttonObj {
+    text:string
+}
+
+
+export const Button= (props: buttonObj)=>{
+
+    const text= props.text;
+
+    return(
+        <button className="p-6 bg-purple-600 text-white font-bold text-xl rounded-2xl">
+            {text}
+        </button>
+    )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+
+
+ const Navbar = ()=>{
+
+    return(
+        <div className="flex flex-row justify-evenly w-1/2 mx-auto border-8 rounded-full mt-5 p-7">
+            <div>Raj Cherukuru</div>
+            <div>Home</div>
+            <div>Projects</div>
+            <div>Resume</div>
+        </div>
+    )
+}
+
+export default Navbar
